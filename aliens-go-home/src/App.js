cb0c6a2b2b9fd9689647f0a3b1439664d00fd3ed
phihainguyen <phihai93@gmail.com>
2020-03-05 14:02:21 -0800

    update

--- aliens-go-home/src/App.js
+++ aliens-go-home/src/App.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
 
 import './App.css';
-
+import PropTypes from 'prop-types';
+import Canvas from './components/Canvas';
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
-				<h1>We will create an awesome game with React, Redux, and SVG!</h1>
+				<Canvas />
 			</div>
 		);
 	}
 }
+
+App.propTypes = {
+	message: PropTypes.string.isRequired
+};
 
 export default App;
