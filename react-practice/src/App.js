f60d8d0a56232472c57990c614a67f59000dfdfc
phihainguyen <phihai93@gmail.com>
2020-03-07 22:19:14 -0800

    update radium

--- react-practice/src/App.js
+++ react-practice/src/App.js
@@ -7,14 +7,17 @@ class App extends Component {
 		persons: [
 			{
 				name: 'Max',
+				id: 1,
 				age: 22
 			},
 			{
 				name: 'Mark',
+				id: 2,
 				age: 24
 			},
 			{
 				name: 'Molly',
+				id: 3,
 				age: 17
 			}
 		]
@@ -24,67 +27,117 @@ class App extends Component {
 			persons: [
 				{
 					name: newName,
+					id: 1,
 					age: 22
 				},
 				{
 					name: 'Joe',
+					id: 2,
 					age: 24
 				},
 				{
 					name: 'Jacky',
+					id: 3,
 					age: 17
 				}
-			]
+			],
+			showPerson: true
 		});
 	};
 
-	nameChangeHandler = (event) => {
-		this.setState({
-			persons: [
-				{
-					name: 'Max',
-					age: 22
-				},
-				{
-					name: event.target.value,
-					age: 24
-				},
-				{
-					name: 'Jacky',
-					age: 17
-				}
-			]
+	nameChangeHandler = (event, id) => {
+		const personIndex = this.state.persons.findIndex((p) => {
+			return p.id === id;
 		});
+		const person = { ...this.state.persons[personIndex] };
+		//below is the second way to cope the object without mutating state
+		// const person = Object.assign({}, this.state.persons[personIndex]);
+
+		person.name = event.target.value;
+
+		const persons = [ ...this.state.persons ];
+		persons[personIndex] = person;
+
+		this.setState({ persons: persons });
+		// this.setState({
+		// 	persons: [
+		// 		{
+		// 			name: 'Max',
+		// 			age: 22
+		// 		},
+		// 		{
+		// 			name: event.target.value,
+		// 			age: 24
+		// 		},
+		// 		{
+		// 			name: 'Jacky',
+		// 			age: 17
+		// 		}
+		// 	]
+		// });
+	};
+	togglePerson = () => {
+		const doesShow = this.state.showPerson;
+
+		this.setState({ showPerson: !doesShow });
+	};
+	deletePersonHandler = (personIndex) => {
+		// const person = this.state.persons.slice();
+		const person = [ ...this.state.persons ];
+
+		person.splice(personIndex, 1);
+		this.setState({ persons: person });
 	};
 	//we can look at the 2 Person tags that binds the this keyword and both does the same thing, they are both able to pass through the props referencing the correct this, thanks to arrow function able to inherits its lexical scope "this", even though it may be easier to write it can be inefficient and make yur app slow, since it may make your app rerender a lot
 	render() {
 		const styles = {
-			backgroundColor: 'yellow',
+			backgroundColor: 'green',
+			color: 'white',
 			padding: '10px',
 			fontSize: '20px',
 			borderRadius: '7px',
-			cursor: 'pointer'
+			cursor: 'pointer',
+			margin: '10px'
 		};
-		const style2 = {
-			...styles,
-			color: 'black'
-		};
+		let person;
+		if (this.state.showPerson) {
+			person = (
+				<div className="personContainer">
+					{this.state.persons.map((person, index) => {
+						return (
+							<Person
+								nameChanged={(event) => this.nameChangeHandler(event, person.id)}
+								key={person.id}
+								name={person.name}
+								age={person.age}
+								click={() => this.deletePersonHandler(index)}
+							/>
+						);
+					})}
+				</div>
+			);
+			styles.backgroundColor = 'red';
+		} else {
+			person = null;
+		}
+		let classes = [];
+		if (this.state.persons.length <= 2) {
+			classes.push('red');
+		}
+		if (this.state.persons.length <= 1) {
+			classes.push('bold');
+		}
 		return (
 			<div className="App">
 				<header className="App-header">
+					<p className={classes.join(' ')}>This is Working!</p>
 					<button style={styles} onClick={() => this.switchNameHandler('Monkey D Luffy')}>
 						Switch Name
 					</button>
-					<Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-					<Person
-						name={this.state.persons[1].name}
-						age={this.state.persons[1].age}
-						click={this.switchNameHandler.bind(this, 'Marshall D Teach')}
-						nameChanged={this.nameChangeHandler}
-					>
-						<p style={style2}>you can also click me to switch </p>
-					</Person>
-					<Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+					<button onClick={this.togglePerson} style={styles}>
+						Toggle People
+					</button>
+					{person}
 				</header>
 			</div>
 		);
--- react-practice/src/Person/Person.js
+++ react-practice/src/Person/Person.js
@@ -3,9 +3,9 @@ import './Person.css';
 const person = (props) => {
 	return (
 		<div className="Person">
-			<h1>{props.name}</h1>
+			<h1 onClick={props.click}>{props.name}</h1>
 			<p>{props.age}</p>
-			<p onClick={props.click}>{props.children}</p>
+			<p>{props.children}</p>
 			<input type="text" onChange={props.nameChanged} value={props.name} />
 		</div>
 	);
